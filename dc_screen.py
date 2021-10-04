41c607492d7f6b383e186f6153f3c639543bca7e
thomaskhong <thomaskhong@users.noreply.github.com>
2021-10-04 18:46:06 +0100

    update

--- dc_screen.py
+++ dc_screen.py
@@ -10,7 +10,7 @@ import numpy as np
 def capture_screenshot():
     # Capture entire screen
     with mss() as sct:
-        monitor = sct.monitors[2]
+        monitor = sct.monitors[1]
         sct_img = sct.grab(monitor)
         # Convert to PIL/Pillow Image
         return Image.frombytes('RGB', sct_img.size, sct_img.bgra, 'raw', 'BGRX')
